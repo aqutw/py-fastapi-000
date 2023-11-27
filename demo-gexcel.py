19030714956f7250d11197d53410a198acfeb650
aqutw <aqutw@aqutw>
2023-11-27 14:28:39 +0800

    integrate google-spreadsheet data into FastAPI with gspread

--- demo-gexcel.py
+++ demo-gexcel.py
@@ -1,16 +1,35 @@
-import gspread
+import gspread, numpy, pandas
 from oauth2client.service_account import ServiceAccountCredentials as SAC
 
 Json = "celtic-sunlight-406403-355cac21e116.json"
 Url = ["https://spreadsheets.google.com/feeds"]
 Connect = SAC.from_json_keyfile_name(Json)
 GoogleSheets = gspread.authorize(Connect)
-Sheet = GoogleSheets.open_by_key("1IDAJHxjL4LJ0-F_gjxEX_UpIC_f8-PL3OWK3NmYPuKo")
+Sheet = GoogleSheets.open_by_key(
+    "1IDAJHxjL4LJ0-F_gjxEX_UpIC_f8-PL3OWK3NmYPuKo")
 Sheets = Sheet.sheet1
 dataTitle = ["name", "account", "password"]
 datas = ["Alex", "qaz", "111"]
-Sheets.append_row(dataTitle)
-Sheets.append_row(datas)
-print("寫入成功")
+# Sheets.append_row(dataTitle)
+# Sheets.append_row(datas)
+# print("寫入成功")
 print(Sheets.get_all_values())
+# 整行
+print(Sheets.row_values(1))
+# 整列
+print(Sheets.col_values(1))
+print(Sheets.get_all_records())
 
+Sheets.format('A1:B1', {'textFormat': {'bold': True}})
+cell = Sheets.find("qaz")
+if cell:
+  print("cell=", cell.row, cell.col)
+Sheets.update("A5:A5", "Ryan2")
+print(Sheets.get_all_records())
+
+# pandas
+dataframe = pandas.DataFrame(Sheets.get_all_records())
+print(dataframe)
+# numpy
+array = numpy.array(Sheets.get_all_values())
+print(array)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
 from typing import Union
 
 from fastapi import FastAPI
@@ -12,3 +14,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/test-gspread")
+def test_gspread():
+  Json = "celtic-sunlight-406403-355cac21e116.json"
+  Connect = SAC.from_json_keyfile_name(Json)
+  GoogleSheets = gspread.authorize(Connect)
+  Sheet = GoogleSheets.open_by_key(
+      "1IDAJHxjL4LJ0-F_gjxEX_UpIC_f8-PL3OWK3NmYPuKo")
+  Sheets = Sheet.sheet1
+  return Sheets.get_all_records();
